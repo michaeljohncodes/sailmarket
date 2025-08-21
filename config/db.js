08bbdb309f000c20d01e41967f11cd84acb4553e
michaeljohncodes <michaelchukwunyem@gmail.com>
2025-08-21 16:25:02 +0100

    first commit

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const MONGODB_LIVE_URI =
+  "mongodb+srv://michaelchukwunyem:Elemental324247@test.kq5jxjl.mongodb.net/?retryWrites=true&w=majority&appName=Test";
+// const MONGODB_LOCAL_URI = "mongodb://localhost:27017/sailmarket";
+
+mongoose
+  .connect(MONGODB_LIVE_URI)
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log("Failed to connect to Database");
+  });
+
+module.exports = mongoose;
